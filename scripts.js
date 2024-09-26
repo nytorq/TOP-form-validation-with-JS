45d23d1cf4038360eb54cfdcfac66eef21550f6a
nytorq <charlieprator@gmail.com>
2024-09-26 17:14:58 -0600

    Initial files and plan

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// scripts.js
+console.log('JavaScript is working!');
+
+
+/* 
+
+PLAN:
+- Create form with email, country, zip code, password, and password confirmation
+- Create styles invalid styles
+- Work on validation criteria:
+    - Email
+        - Must not be empty
+    - Country
+        - Must not be empty
+    - Zip
+        - Must not be empty
+    - Password
+        - Must not be empty
+    - Password confirm
+        - Must not be empty
+        - Must match password
+    - Form-wide requirements
+        - All inputs must have valid
+
+REQUIREMENTS:
+- All validation should be done in JS files.
+- Upon submission and valid form entries, give user success message.
+- Validation should be done upon blur of any input
+- Styling should be done via :valid and :invalid
+
+*/
