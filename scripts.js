9c2fe41b610ed7d9b2f32f9ca45909e640e9731b
nytorq <charlieprator@gmail.com>
2024-09-26 17:58:28 -0600

    More planning/thinking

--- scripts.js
+++ scripts.js
@@ -16,11 +16,17 @@ PLAN:
         - Must not be empty
     - Password
         - Must not be empty
+        - Must be between 12 - 16 chars long
+        - Must contain:
+            - Uppercase letters (A-Z)
+            - Lowercase letters (a-z)
+            - Numbers (0-9)
+            - Special characters (e.g., !@#$%^&*)
     - Password confirm
         - Must not be empty
         - Must match password
-    - Form-wide requirements
-        - All inputs must have valid
+Form-wide requirements
+    - All inputs must have valid contents
 
 REQUIREMENTS:
 - All validation should be done in JS files.
@@ -28,4 +34,20 @@ REQUIREMENTS:
 - Validation should be done upon blur of any input
 - Styling should be done via :valid and :invalid
 
+NOTES
+Use built-in form validation for:
+    - All inputs to be required
+    - min length on email > 4 characters (i.e. "@" and roughly ".tld")
+    - min and max length on country to ensure 3 characters, i.e. USA or FRA.
+    - type should be correctly assigned
+Where to use pattern attribute?
+    - Email
+    - Country
+    - Zip - this'll be hard to create from scratch
+    - Password- def
+- Use validationMessage for password requirements
+- Use the validity object to get details on what's wrong exactly.
+- Custom messages are done through setCustomValidity()
+- Might need novalidate to turn off browser validation and to force the form to rely upon my own validation
+- Will limit countries to USA, FRA, and GER.
 */
